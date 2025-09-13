f2a44a4ae03453b1ff9e2dabbe7041574f3647d7
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-09-13 18:37:21 +0530

    add default backend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,20 @@ app.use(express.json());
 // baseurl 
 const baseUrl = "/api/v1"
 //routes
-app.use(baseUrl,urlRoutes)
+app.use(baseUrl, urlRoutes)
 
-// 404 Handler (Not Found)
+
+// default route 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the URL Shortener API 🚀",
+    endpoints: {
+      "POST /api/v1/shorten": "Create a shortened URL",
+      "GET /:shortCode": "Redirect to original URL using shortCode",
+    },
+  });
+});
+
 app.all(/(.*)/, (req, res) => {
   res.status(404).json({ message: "Invalid Request URL!" });
 });
